810a04c5d1c1262cd553f25c03074f663354d5c4
Jo-Theo <theodorevanrenterghem@gmail.com>
2025-07-12 08:44:14 +0200

    not to much avanced

--- Jo_dev/4_exploration.R
+++ Jo_dev/4_exploration.R
@@ -22,3 +22,20 @@ behaviours <- read.csv2("Jo_dev/Data/bouted_behaviours_SPPA2024_1.csv")
 ########################
 ### Data exploration ###
 ########################
+
+behaviours %>% 
+  split(.$Session) %>% 
+  map_dfr(~data.frame(
+    Session = .x$Session[1],
+    Prop_na = .x %>% 
+      pull(F_in) %>% 
+      is.na() %>% 
+      mean() %>% 
+      round(3)
+  ))
+
+
+
+for (i in 1:dim(test)[1]){
+  
+}
