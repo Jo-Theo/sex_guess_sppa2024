5a3ef302f096003f3b1cc2944c01cc6b221c5e14
Jo-Theo <theodorevanrenterghem@gmail.com>
2025-07-16 06:04:50 +0200

    unk

--- Jo_dev/4_exploration.R
+++ Jo_dev/4_exploration.R
@@ -38,30 +38,33 @@ behaviours %>%
       round(3)
   ))
 
-
-behaviours %>% 
-  filter(Session ==  unique(Session)[4]) %>% 
-  pull(F_in)
-
-two_next_diff_value <- function(vec,pos){
-  pos1 <- pos+1
-  while (vec[pos] == vec[pos1]) {
-    if(pos1 > length(vec)){
-      return(c(NA,NA))
-    }
-    pos1 <- pos1+1
-  }
-  pos2 <- pos1 + 1
-  while (vec[pos1] == vec[pos2]) {
-    if(pos2 > length(vec)){
-      return(c(pos1,NA))
-    }
-    pos2 <- pos2+1
-  }
-  return(c(pos1,pos2))
-}
+test <- behaviours %>%
+  filter(Session ==  unique(Session)[4])
 
 
-for (i in 1:dim(test)[1]){
-  
-}
+# test <- behaviours %>% 
+#   filter(Session ==  unique(Session)[4]) %>% 
+#   pull(F_in) %>% 
+#   as.character() %>% 
+#   replace_na("NA")
+# 
+# two_next_diff_value <- function(vec,pos){
+#   pos1 <- pos+1
+#   while (vec[pos] == vec[pos1]) {
+#     if(pos1 > length(vec)){
+#       return(c(NA,NA))
+#     }
+#     pos1 <- pos1+1
+#   }
+#   pos2 <- pos1 + 1
+#   while (vec[pos1] == vec[pos2]) {
+#     if(pos2 > length(vec)){
+#       return(c(pos1,NA))
+#     }
+#     pos2 <- pos2+1
+#   }
+#   return(c(pos1,pos2))
+# }
+# 
+# two_next_diff_value(test,2)
+
