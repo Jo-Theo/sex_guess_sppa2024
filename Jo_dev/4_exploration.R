c96768871fc3eb798d82258c17d0940bcbc584bd
Jo-Theo <theodorevanrenterghem@gmail.com>
2025-07-17 05:16:44 +0200

    create individual bout table

--- Jo_dev/4_exploration.R
+++ Jo_dev/4_exploration.R
@@ -35,19 +35,82 @@ sub_session_behaviours$Sub_session %>%
 
 i <- 1
 
-get_bout_ind <- function(behaviours, na_last_first = FALSE){
+.get_bout_ind <- function(behaviours, na_last_first = FALSE){
   behaviours %>% 
     mutate(male_bout =  .indiv_bout_nb(behaviours$M_in, na_last_first),
            female_bout = .indiv_bout_nb(behaviours$F_in, na_last_first))
 } 
 
+.contract_bout <- function(behaviours,male_bird = T){
+  if(male_bird){
+    split_by <- behaviours$male_bout
+    to_erase <- c("F_in","U_in","male_bout", "female_bout")
+    rename_this <- "M_in"
+    sex <- "M"
+  }else{
+    split_by <- behaviours$female_bout
+    to_erase <- c("M_in","U_in","male_bout", "female_bout")
+    rename_this <- "F_in"
+    sex <- "F"
+  }
+  behaviours %>% 
+    split(split_by) %>% 
+    map_dfr(function(bout){
+      res_bout <- bout[1,]
+      res_bout[to_erase] <- NULL
+      res_bout$Sex <- sex
+      res_bout$Nb_bird <- mean(bout$Nb_bird)
+      res_bout$Bout_length<- mean(bout$Bout_length)
+      res_bout %>% 
+        rename_at(rename_this,~'Bird_in')
+    })
+  
+}
+
 
 behaviours <- sub_session_behaviours %>% 
   filter(Sub_session == unique(Sub_session)[i])
+bout_ind_behaviours <- .get_bout_ind(behaviours,T)
 
-get_bout_ind(behaviours,T)
+.contract_bout <- function(behaviours,male_bird = T){
+  if(male_bird){
+    split_by <- behaviours$male_bout
+    to_erase <- c("F_in","U_in","male_bout", "female_bout")
+    rename_this <- "M_in"
+    sex <- "M"
+  }else{
+    split_by <- behaviours$female_bout
+    to_erase <- c("M_in","U_in","male_bout", "female_bout")
+    rename_this <- "F_in"
+    sex <- "F"
+  }
+  behaviours %>% 
+    split(split_by) %>% 
+    map_dfr(function(bout){
+      res_bout <- bout[1,]
+      res_bout[to_erase] <- NULL
+      res_bout$Sex <- sex
+      res_bout$Nb_bird <- mean(bout$Nb_bird)
+      res_bout$Bout_length<- mean(bout$Bout_length)
+      res_bout %>% 
+        rename_at(rename_this,~'Bird_in')
+    })
+  
+}
+  
+.contract_bout(bout_ind_behaviours,F)
 
-
+bout_ind_behaviours %>% 
+  split(.$male_bout) %>% 
+  map_dfr(function(bout){
+    res_bout <- bout[1,]
+    res_bout[c("F_in","U_in","male_bout", "female_bout")] <- NULL
+    res_bout$Sex <- "M"
+    res_bout$Nb_bird <- mean(bout$Nb_bird)
+    res_bout$Bout_length<- mean(bout$Bout_length)
+    res_bout %>% 
+      rename(Bird_in = M_in)
+  })
 
 
 
