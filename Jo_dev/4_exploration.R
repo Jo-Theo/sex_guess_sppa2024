c4d788edd71ddee09d17310679fe2ec51a2403a0
Jo-Theo <theodorevanrenterghem@gmail.com>
2025-07-17 03:09:42 +0200

    Comment about goal

--- Jo_dev/4_exploration.R
+++ Jo_dev/4_exploration.R
@@ -18,4 +18,16 @@ behaviours <- read.csv2("Jo_dev/Data/bouted_behaviours_SPPA2024_1.csv")
 ### Data exploration ###
 ########################
 
-mini_behaviours <- sub_session_by_session(behaviours, length_min = 2)
+# defining sub_session
+sub_session_behaviours <- sub_session_by_session(behaviours, length_min = 1)
+
+### So we have all sub session but we want to get rid of first and last states of each session (for male in/out and female in/out). 
+# We do this because we are not sure that the stating state actually started at the beginning of the sub_session 
+
+# Looking for number of long sessions 
+sub_session_behaviours$Sub_session %>% 
+  as.factor() %>% 
+  summary() %>% 
+  sort()
+
+
